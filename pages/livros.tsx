bb48fe23919e6e91e54afc73d7ca447a0ffbcec8
Diogo Bratti <dbratti@gmail.com>
2023-12-04 14:29:49 -0300

    books

--- pages/livros.tsx
+++ pages/livros.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Livros: NextPage = () => {
     return (
         <>
-            <div className="flex flex-col grow items-center">
+            <div className="flex flex-col grow items-center text-white">
                 <Link href="/documents/livros/Algorithms-Dasgupta-Papadimitriou-Vazirani.pdf">Algorithms - Dasgupta Papadimitriou Vazirani</Link>
                 <Link href="/documents/livros/As_Armas_da_Persuasao_-_Robert_B_Cialdini.pdf">As Armas da Persuasao - Robert B. Cialdini</Link>
                 <Link href="/documents/livros/Cracking-the-Coding-Interview-6th-Edition-189-Programming-Questions-and-Solutions.pdf">Cracking the Coding Interview 6th Edition</Link>
